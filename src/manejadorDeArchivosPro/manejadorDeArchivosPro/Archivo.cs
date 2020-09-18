49ae8de465c1e93e3aac793ca1a195fd2aeddaad
Pedejeca135 <pjco90@hotmail.com>
2020-09-18 13:41:05 -0500

    primera realizacion de la vista para el proyecto

---
+++ src/manejadorDeArchivosPro/manejadorDeArchivosPro/Archivo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejadorDeArchivosPro
+{
+    class Archivo
+    {
+        #region Variables_De_Instancia
+        private long cabecera;
+        private string nombre;
+        private List<Entidad> entidades;
+        #endregion
+
+        #region Constructores
+        public Archivo(string nombre)
+        {
+            this.cabecera = -1;
+            this.nombre = nombre;
+            this.entidades = new List<Entidad>();
+        }
+        #endregion
+
+        #region Geters&Seters
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+        public long Cabecera
+        {
+            get { return this.cabecera; }
+        }
+        public List<Entidad> Entidades
+        {
+            get { return this.entidades; }
+        }
+        #endregion
+
+        #region Metodos
+       
+        #endregion
+    }
+}
---
+++ src/manejadorDeArchivosPro/manejadorDeArchivosPro/Entidad.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejadorDeArchivosPro
+{
+    class Entidad
+    {
+
+        #region Variables de Instancia
+        private string nombre;//Nombre de la entidad
+        private long dirActual;//Direccion en el archivo de la entidad
+        private long dirAtributos;//Direccion en el archivo de los atributos
+        #endregion
+
+        #region Constructores
+        public Entidad(string nombre, long dirActual, long dirAtributos, long dirRegistros, long dirSig)
+        {
+            this.nombre = nombre;
+            this.dirActual = dirActual;
+            this.dirAtributos = dirAtributos;
+        }
+        #endregion
+
+        #region Gets & Sets
+
+        public long DirActual
+        {
+            get { return this.dirActual; }
+        }
+
+        public long DirAtributos
+        {
+            get { return this.dirAtributos; }
+        }
+        #endregion
+
+        #region Metodos
+
+
+        #endregion
+
+    }
+}
